7c1f60f2ada7e1d64b2d6553cb991ae4f87cbf43
Tony Edwards <tony@plymouthsoftware.com>
2015-08-05 10:54:14 +0100

    Indents javascript

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ var welcomeMessage = "Function called on ready";
 
 
 // Called once the document has fully loaded
-$(document).ready(
-		function(){ 	welcome(welcomeMessage);
+$(document).ready( function(){ 	
+		welcome(welcomeMessage);
+
 		//determine backstretch image
-		var mx = applyBackstretchImg();
+		applyBackstretchImg();
 });
 
 
@@ -21,14 +22,14 @@ var welcome = function(msg){
 }
 
 function applyBackstretchImg() {
-	// apply different image depending on if phone/tablet/other
-	if (window.innerWidth < 480) {
-								$.backstretch("images/stone-roots-mobile.jpg");
-						}
-	else if ( window.innerWidth <= 768) {
-								$.backstretch("images/stone-roots-tablet.jpg");
-						}
-	else if   ( window.innerWidth > 768) {
-													$.backstretch("images/stone-roots-home.jpg");
-						}
+		// apply different image depending on if phone/tablet/other
+		if (window.innerWidth < 480) {
+				$.backstretch("images/stone-roots-mobile.jpg");
+		}
+		else if ( window.innerWidth <= 768) {
+				$.backstretch("images/stone-roots-tablet.jpg");
+		}
+		else if   ( window.innerWidth > 768) {
+				$.backstretch("images/stone-roots-home.jpg");
+		}
 }
