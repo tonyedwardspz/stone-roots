589d86dfe681c55650fa0cd0324dc8fbf7c2775c
Tony Edwards <tony@plymouthsoftware.com>
2015-08-05 15:15:00 +0100

    Adds method for detecting orientation

--- js/script.js
+++ js/script.js
@@ -1,36 +1,51 @@
-var welcomeMessage = "Function called on ready";
+var orientation = "";
 
 
 // Called once the document has fully loaded
-$(document).ready(function(){ 	
-		welcome(welcomeMessage);
-
-		//determine backstretch image
-		applyBackstretchImg();
-});
-
-
-// Called when the browser window resizes
-$(window).resize(function(){
-	console.log('Resize')
+$(document).ready(function() { 	
+	detectOrientation();
 	applyBackstretchImg();
 });
 
 
-// Storing functions in variables caches them in browser memory
-var welcome = function(msg){
-	console.log(msg);
+// Called when the browser window resizes
+$(window).resize(function() {
+	detectOrientation();
+	applyBackstretchImg();
+});
+
+
+// apply different image depending on screen width
+var applyBackstretchImg = function() {
+	if (window.innerWidth < 480) {
+		$.backstretch("images/stone-roots-mobile.jpg");
+	}
+	else if (window.innerWidth <= 768) {
+		$.backstretch("images/stone-roots-tablet.jpg");
+	}
+	else {
+		$.backstretch("images/stone-roots-home.jpg");
+	}
 }
 
-function applyBackstretchImg() {
-		// apply different image depending on if phone/tablet/other
-		if (window.innerWidth < 480) {
-				$.backstretch("images/stone-roots-mobile.jpg");
+
+// Use match media for orientation detection.
+var detectOrientation = function() {
+	var mql = window.matchMedia("(orientation: portrait)");
+
+	if(mql.matches) {
+		orientation = "portrait"
+	} else {
+		orientation = "landscape"
+	}
+
+	// Add a media query change listener
+	mql.addListener(function(m) {
+		if(m.matches) {
+			orientation = "portrait"
 		}
-		else if ( window.innerWidth <= 768) {
-				$.backstretch("images/stone-roots-tablet.jpg");
+		else {
+			orientation = "landscape"
 		}
-		else if   ( window.innerWidth > 768) {
-				$.backstretch("images/stone-roots-home.jpg");
-		}
+	});
 }
